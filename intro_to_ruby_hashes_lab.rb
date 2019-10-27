374c8f45f9dc5f784535af091bac8deb40c5632b
Sean Good <tree662@protonmail.com>
2019-10-27 18:40:03 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,9 +26,9 @@ def monopoly_with_third_tier
       },
       :names => {
         :reading_railroad => {},
-        :pennsylvania_railroad => 200,
-        :b_and_o_railroad => 400,
-        :shortline_railroad => 800
+        :pennsylvania_railroad => {},
+        :b_and_o_railroad => {},
+        :shortline_railroad => {}
       }
     }
   }
