816be8f32e3e184828e833f3f9ed1ea737f7bd99
Sean Good <tree662@protonmail.com>
2019-10-27 18:10:45 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,8 +15,8 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
+  binding.pry
   monopoly = {
-    binding.pry
     :railroads => {
       :pieces => (4),
       :rent_in_dollars => {
